3b76e95d6f8abb0498331aaabef5259997026d98
Emily Ramirez <emrami01@gmail.com>
2023-11-14 18:04:07 -0700

    updated offsets for images

--- image-slicer/image-slicer.py
+++ image-slicer/image-slicer.py
@@ -7,6 +7,7 @@ BASE_PATH = os.getenv("BASE_PATH")
 OUTPUT_PATH = os.getenv("OUTPUT_PATH")
 PAGES = int(os.getenv("PAGES"))
 SPLITS = int(os.getenv("SPLITS"))
+OFFSET = int(os.getenv("OFFSET"))
 
 # # these are how much to move the slices up to align the images
 # offset_1 = 0
@@ -25,26 +26,38 @@ def main(is_bulk=False, page=0):
         if iter_pages == 1:
             p = page - 1
         image_paths = get_image_paths(p + 1, SPLITS)
-        print(image_paths)
+        images = []
+        for path in image_paths:
+            images.append(Image.open(path))
+
+        multi_concat_vertical(images, OFFSET).save(f"{OUTPUT_PATH}0{p+1}.jpg")
 
 
 def get_image_paths(page, SPLITS):
     image_paths = []
     for s in range(SPLITS):
-        image_paths.append(f"{page}-{s+1}.jpg")
+        image_paths.append(f"{BASE_PATH}{page}-{s+1}.jpg")
     return image_paths
 
 
-def concat_vertical(im1, im2, offset):
-    w = max(im1.size[0], im2.size[0])
-    h = im1.size[1] + im2.size[1]
-    im = Image.new("RGBA", (w, h))
+def concat_vertical(im1, im2, offset=0):
+    w = max(im1.width, im2.width)
+    h = im1.height + im2.height - offset
+    im = Image.new("RGB", (w, h))
 
     im.paste(im1, (0, 0))
-    im.paste(im2, (im1.size[0], 0))
+    im.paste(im2, (0, im1.height - offset))
 
     return im
 
 
+# source: https://note.nkmk.me/en/python-pillow-concat-images/
+def multi_concat_vertical(im_list, offset=0):
+    _im = im_list.pop(0)
+    for im in im_list:
+        _im = concat_vertical(_im, im, offset)
+    return _im
+
+
 if __name__ == "__main__":
-    main(is_bulk=False, page=1)
+    main(is_bulk=True, page=1)
